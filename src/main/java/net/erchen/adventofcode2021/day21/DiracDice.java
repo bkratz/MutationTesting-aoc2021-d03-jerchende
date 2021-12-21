999059bdf5ccfaf9e9b6c17fe3984903827c5cfa
Johannes Erchen <johannes@erchen-it.de>
2021-12-21 12:08:34 +0100

    DAY-21 move classes from part 1 to inline classes again

--- src/main/java/net/erchen/adventofcode2021/day21/DiracDice.java
+++ src/main/java/net/erchen/adventofcode2021/day21/DiracDice.java
@@ -1,5 +1,7 @@
 package net.erchen.adventofcode2021.day21;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
@@ -20,6 +22,37 @@ public class DiracDice {
     public void playUntilPlayerHas1000Points() {
         while (player1.move(dice.rollTreeTimes()) < pointsToWin && player2.move(dice.rollTreeTimes()) < pointsToWin) {
             // empty
+        }
+    }
+
+    @Getter
+    public static class Dice {
+        private int rolls;
+        private int value;
+        private final int maxValue = 100;
+
+        public int roll() {
+            rolls++;
+            value = value == maxValue ? 1 : value + 1;
+            return value;
+        }
+
+        public int rollTreeTimes() {
+            return roll() + roll() + roll();
+        }
+    }
+
+    @Getter
+    @Builder
+    @EqualsAndHashCode
+    public static class Player {
+        private int points;
+        private int position;
+
+        public int move(int diceValue) {
+            position = (position + diceValue - 1) % 10 + 1;
+            points += position;
+            return points;
         }
     }
 
--- src/test/java/net/erchen/adventofcode2021/day21/DiracDiceTest.java
+++ src/test/java/net/erchen/adventofcode2021/day21/DiracDiceTest.java
@@ -7,6 +7,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DiracDiceTest {
 
     @Test
+    void shouldRollDeterministicDice() {
+        var dice = new DiracDice.Dice();
+
+        assertThat(dice.roll()).isEqualTo(1);
+        assertThat(dice.roll()).isEqualTo(2);
+        assertThat(dice.roll()).isEqualTo(3);
+
+        assertThat(dice.rollTreeTimes()).isEqualTo(15);
+
+        assertThat(dice.getRolls()).isEqualTo(6);
+    }
+
+    @Test
+    void shouldMove() {
+        var player = new DiracDice.Player(0, 9);
+
+        player.move(1);
+        assertThat(player.getPosition()).isEqualTo(10);
+        assertThat(player.getPoints()).isEqualTo(10);
+
+        player.move(1);
+        assertThat(player.getPosition()).isEqualTo(1);
+        assertThat(player.getPoints()).isEqualTo(11);
+    }
+
+    @Test
     void playUntilPlayerHas1000Points_Sample() {
         var game = new DiracDice(4, 8);
         game.playUntilPlayerHas1000Points();
